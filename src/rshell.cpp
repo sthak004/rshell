0153aed843b05c44db3fbd581c04f9c038a4923c
Stavan Thaker <sthak004@hammer.cs.ucr.edu>
2015-04-14 19:36:32 -0700

    added getlogin and gethostname functionality

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string>
 using namespace std;
 
+
 int main(int argc, char** argv){
-    cout << "This is.....";
-    cout << "WORKING...!" << endl;
-    return 0;
+        char *user = getlogin(); //syscall for getting username
+        //perror check for getlogin()
+        if(user == NULL){
+                perror("getlogin");
+                exit(1);
+        }        
+        char hostname[100]; //create a char array for the hostname
+        if((gethostname(hostname, sizeof(hostname))) == -1){
+        perror("hostname");
+        exit(1);
+        }              
+   
+   
+        cout << user << '@' << hostname << '$' <<  endl; //output user and host
+        return 0;
 }
