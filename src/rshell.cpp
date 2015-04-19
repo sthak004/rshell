382448841650a97f7a610079e8c931f52765f13a
Stavan Thaker <sthak004@hammer.cs.ucr.edu>
2015-04-19 09:57:09 -0700

    added splitting argument and flag functionaility

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -29,6 +29,32 @@ void displayPrompt(){
 
 }
 
+
+
+void splitSpaces(char* argument, char **pendingArgs, int pos){
+    string temp(argument);
+    //int count = 1; //used for debugging
+    for(unsigned int i = 0; i < temp.size(); ++i){
+        if(temp.at(i) == ' '){
+            int j = i;
+            while(temp.at(j) == ' '){
+                j++;
+            }
+            string part1 = temp.substr(0, i);
+            //cout << "PART" << count << ": "  << part1 << endl;
+            pendingArgs[pos] = const_cast<char*>(part1.c_str());
+            string part2 = temp.substr(j, temp.size());
+            //cout << "PART" << count++ << ": " << part2  << endl;
+            pendingArgs[pos] = const_cast<char*>(part2.c_str());
+        }
+    }
+}
+
+
+
+
+
+
 int executeCommand(char **args){
     int pid = fork();
 
@@ -57,7 +83,7 @@ int extractCommands(string &commandLine, char **cmds){
     const char* args = commandLine.c_str(); //the user input is converted to a const char* as we need it for execvp
     char* args_Mutatable = const_cast<char*>(args); //it needs to be mutable for us to tokenize it
     char *single_command;
-    single_command = strtok(args_Mutatable, " ;&|"); //execute strtok with delimeters
+    single_command = strtok(args_Mutatable, ";&|"); //execute strtok with delimeters
     
     int numOfArgs = 0; //when you add one --> argv[1]
 
@@ -67,7 +93,7 @@ int extractCommands(string &commandLine, char **cmds){
         cmds[numOfArgs] = strdup(single_command); //this processes each command into the command array for execution
         cout << "at position " << numOfArgs << ": " << cmds[numOfArgs];
         cout << endl;
-        single_command = strtok(NULL, " ;&|");
+        single_command = strtok(NULL, ";&|");
         numOfArgs++;
     }
 
@@ -92,14 +118,20 @@ vector<string>  extractConnectors(string inputLine){
     return cnctors;
 }
 
+
+
 void run(vector<string> &connectors, char **cmds){
-    for(unsigned int i = 0; i < connectors.size(); ++i){
-        if(connectors.at(i) == ";"){
-            if(executeCommand(cmds) != 0){
-                cout << "ERROR: couldn't execute command properly" << endl;   
-            }
+    char *pendingArgs[10000]; //arguments waiting to be executed depending on connector 
+    /*for(unsigned int i = 0; i < connectors.size(); ++i){
+        if(connectors.at(i) == ";" && hasSpace(cmds[i])){
+            cout << "BEFORE: " << cmds[i] << endl;
+            pendingArgs[i] = cmds[i];
+            cout << "AFTER: " << pendingArgs[i] << endl;
+            executeCommand(pendingArgs);
         }
-    }
+    }*/
+    int i = 0;
+    splitSpaces(cmds[0], pendingArgs, i);
 }
 
 
@@ -132,10 +164,11 @@ int main(){
         if( (strcmp(cmds[0], "exit") == 0) && (numArgs == 1) ) { break; }
 
         
-        run(connectors, cmds);
-        /*if(executeCommand(cmds) != 0){
+        //run(connectors, cmds);
+
+        if(executeCommand(cmds) != 0){
             cout << "Error in executing commands" << endl;
-        }*/
+        }
 
     }while(1);
       
